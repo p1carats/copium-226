b034894dd33684bc0106fd96f0baa3d04c12c66c
Noe Barlet <hello@noebarlet.me>
2022-05-12 11:12:00 +0200

    update choice + json init

--- src/objects/choice.ts
+++ src/objects/choice.ts
@@ -1,10 +1,10 @@
-function choiceDiag() {
-	let dialog = this.rexUI.add.dialog({
+function createChoiceBox(game) {
+	let dialog = game.rexUI.add.dialog({
 		x: 400,
 		y: 300,
-		background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x3e2723),
-		title: this.rexUI.add.label({
-			background: this.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x1b0000),
+		background: game.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x3e2723),
+		title: game.rexUI.add.label({
+			background: game.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x1b0000),
 			space: {
 				left: 15,
 				right: 15,
@@ -13,10 +13,10 @@ function choiceDiag() {
 			}
 		}),
 		choices: [
-			createLabel(this, '3', 0x6a4f4b),
-			createLabel(this, '4', 0x6a4f4b),
-			createLabel(this, '5', 0x6a4f4b),
-			createLabel(this, '6', 0x6a4f4b)
+			createLabel(game, '3', 0x6a4f4b),
+			createLabel(game, '4', 0x6a4f4b),
+			createLabel(game, '5', 0x6a4f4b),
+			createLabel(game, '6', 0x6a4f4b)
 		],
 		space: {
 			title: 25,
@@ -35,7 +35,7 @@ function choiceDiag() {
 	}).layout().popUp(1000);
 	//.drawBounds(this.add.graphics(), 0xff0000)
 
-	this.print = this.add.text(0, 0, '');
+	game.print = game.add.text(0, 0, '');
 	dialog.on('button.click', function (button, groupName, index) {
 		this.print.text += index + ': ' + button.text + '\n';
 	});
@@ -47,7 +47,7 @@ function choiceDiag() {
 	});
 }
 
-var createLabel = function (scene, text, backgroundColor) {
+let createLabel = function (scene, text, backgroundColor) {
 	return scene.rexUI.add.label({
 		background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, backgroundColor),
 		text: scene.add.text(0, 0, text, {
@@ -60,4 +60,8 @@ var createLabel = function (scene, text, backgroundColor) {
 			bottom: 10
 		}
 	});
+}
+
+export default function choiceBox(game) {
+	createChoiceBox(game);
 }
